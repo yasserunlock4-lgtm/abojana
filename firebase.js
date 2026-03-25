c0704015966890d07c17bc9032c64d295bcba0db
yasserunlock4-lgtm <yasserunlock4@gmail.com>
2026-03-26 02:33:04 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,38 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getDatabase, ref, get, set } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  get,
+  set,
+  update,
+  push,
+  query,
+  orderByChild,
+  limitToLast
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "*****",
+  apiKey: "AIzaSyDiCrZhKVpkYE3HUeeDkAo7xA9I88uZ1i0",
   authDomain: "abojana-3f0c2.firebaseapp.com",
   databaseURL: "https://abojana-3f0c2-default-rtdb.firebaseio.com",
   projectId: "abojana-3f0c2",
+  storageBucket: "abojana-3f0c2.firebasestorage.app",
+  messagingSenderId: "819681544560",
+  appId: "1:819681544560:web:be2249a51d0c79ee21a7b5",
+  measurementId: "G-23EYSF1BE0"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
-export { ref, get, set };
+const db = getDatabase(app);
+
+export {
+  db,
+  ref,
+  get,
+  set,
+  update,
+  push,
+  query,
+  orderByChild,
+  limitToLast
+};
